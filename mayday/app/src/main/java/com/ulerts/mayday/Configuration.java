e7d67d561cdcbc2f0530f8d1c4c18c303932e100
louis.schimchowitsch <louis.schimchowitsch@cgi.com>
2018-02-03 20:22:48 -0500

    Repush mayday android app

---
+++ mayday/app/src/main/java/com/ulerts/mayday/Configuration.java
@@ -0,0 +1,13 @@
+package com.ulerts.mayday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Configuration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+    }
+}
