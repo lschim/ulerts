3f302ae57f1c8245fb81b82f9993bca6ba9bbe0f
said.mezghanni <said.mezghanni@cgi.com>
2018-02-04 00:50:42 -0500

    rename sos to ulert

--- ulerts-registries/src/main/java/com/hackton/ulertsregistries/web/PublicServerController.java
+++ ulerts-registries/src/main/java/com/hackton/ulertsregistries/web/PublicServerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hackton.ulertsregistries.model.Sos;
+import com.hackton.ulertsregistries.model.Ulert;
 
 @RestController
 @RequestMapping("/publicServer")
@@ -15,13 +15,13 @@ public class PublicServerController {
 	private static final String template = "Hello, %s!";
 	 private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping("/getCase")
-   public Sos getCase(@RequestParam(value="name", defaultValue="World") String name) {
-		  return new Sos(counter.incrementAndGet(),  null, null, null, String.format(template, name));
+	@RequestMapping("/getUlert")
+   public Ulert getUlert(@RequestParam(value="name", defaultValue="World") String name) {
+		  return new Ulert(counter.incrementAndGet(),  null, null, null, String.format(template, name));
    }
 	
-	@RequestMapping("/putCase")
-   public Sos putCase(@RequestParam(value="name", defaultValue="World") String name) {
+	@RequestMapping("/putUlert")
+   public Ulert putUlert(@RequestParam(value="name", defaultValue="World") String name) {
 		  return null;
    }
 
